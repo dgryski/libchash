64378492b03136b3b056183f2d1fd6482c9e0ba7
Eric Herman <eric@freesa.org>
2013-11-09 09:39:09 +0100

    add comment that names are always null-terminated strings
    while data to be hased may be arbitrary byte arrays,
    node names are null-terminated strings

--- chash.h
+++ chash.h
@@ -2,6 +2,10 @@
 
 struct chash_t;
 
+/* keys are required to be null-terminated strings */
 struct chash_t *chash_create(const char **keys, size_t nkeys, size_t replicas);
+
+/* returned key will always be a null-terminated string */
 const char *chash_lookup(struct chash_t *chash, const char *key, size_t len);
+
 void chash_free(struct chash_t *chash);
